f3a17ad5285819867e9cde86b13978a1542ad899
Jesus <migueljesuslara@gmail.com>
2014-03-12 18:31:51 +0100

    modificdado GitAux.java

--- GitAux.java
+++ GitAux.java
@@ -5,8 +5,7 @@
 package prgitejemplo;
 
 /**
- *
- * @author bertoa
+ * @author Miguel Jesus
  */
 public class GitAux {
     
@@ -19,6 +18,7 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
+    	// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
     }
 }
